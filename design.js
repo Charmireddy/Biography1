3c5ca6f5794632c3fa4441e628ec18614c526660
Ch.Charmi Reddy <85749261+Charmireddy@users.noreply.github.com>
2023-07-28 20:26:51 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,29 @@
+ReactDOM.render(
+    <div>
+        
+        
+        <h2>Personal Information</h2>
+        <p>Mahendra Singh Dhoni ( born 7 July 1981) is an Indian professional cricketer. He was captain of the Indian national team in limited-overs formats from 2007 to 2017 and in Test cricket from 2008 to 2014. Dhoni is widely considered one of the greatest cricket captains, wicket-keeper-batsman and finishers in the history of cricket.He plays as a right-handed wicket-keeper-batsman and is known for his calm captaincy and his ability to finish matches in tight situations. He is also the current captain of Chennai Super Kings in the Indian Premier League.</p>
+        <img src="https://c.ndtvimg.com/2022-06/p38nlu98_ms-dhoni-2013-champions-trophy-afp_625x300_23_June_22.jpeg?im=FeatureCrop,algorithm=dnn,width=806,height=605" height="300px" width="300px" />
+        <h2>International Information</h2>
+        <p>Dhoni made his ODI debut on 23 December 2004, against Bangladesh in Chittagong, and played his first Test a year later against Sri Lanka. He played his first T20I also a year later against South Africa. In 2007, he took over the ODI captaincy from Rahul Dravid and he also selected as T20I captain of India in this year. In 2008, he was selected as Test captain. His captaincy record in Tests format was mixed, successfully leading India to a series win against New Zealand in 2008 and the Border-Gavaskar Trophy (home series in 2010 and 2013) against Australia. In other formats, Dhoni is the first cricketer to be a winning captain of all three major ICC trophies, as under his captaincy, India won the 2007 ICC World Twenty20, the 2011 Cricket World Cup, and the 2013 ICC Champions Trophy, the most by any captain. He also led India to victory in the 2010 and 2016 Asia Cup. Additionally, under his leadership, India won the 2010 and 2011 ICC Test Mace and 2013 ICC ODI Championship. He has scored 17,266 runs in International Cricket (including 10,000 plus runs in ODI Internationals).</p>
+        <h2>Domestic Team Information</h2>
+        <img src="https://p.imgci.com/db/PICTURES/CMS/357400/357408.jpg" height="300px" width="300px"/>
+         <p>He is the captain of Chennai Super Kings (CSK) in the Indian Premier League, leading them to the final on 10 occasions and winning in the 2010, 2011, 2018, 2021 and 2023 editions of the league, as well as winning the Champions League T20 two times, in 2010 and 2014. He is the 7th cricketer have scored more than 5,000 runs in the IPL, as well as being the first wicket-keeper to do so.</p>
+        <img src="https://wallpapercave.com/wp/wp12354870.jpg" height="300px" width="300px" />
+        <h2>Awards Achieved</h2>
+        
+            <p>Padma Bhushan</p>
+            <p>Padma Shri</p>
+            <p>ICC Men's ODI Cricketer of the Year</p>
+            <p>ICC Award for Spirit of Cricket</p>
+            <p>ICC Men's ODI Team of the Year</p>
+        
+        
+       
+        <h2>Signature</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/MS_Dhoni_signature.png/1200px-MS_Dhoni_signature.png" height="150px" width="250px" />
+
+        
+    </div>
+    ,document.getElementById("content"));
